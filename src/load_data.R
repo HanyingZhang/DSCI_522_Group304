a1bcff9d003f9b16a4907c4dd5497f3fe3f1c497
Anny <anny@annychih.com>
2020-02-04 07:53:40 -0800

    cleaning up code for readability

--- src/load_data.R
+++ src/load_data.R
@@ -11,11 +11,17 @@ Options:
 
 " -> doc
 
+# Examples:
+# Rscript src/load_data.R 'https://catalogue.data.gov.bc.ca/dataset/5554165d-e365-422f-bf85-4f6e4c9167dc/resource/bcb547f0-8ba7-451f-9e11-10524f4d57a0/download/foundation-skills-assessment-2017-18_to_2018-19.csv' --arg2='data/fsa_2017-2018.csv'
+# Rscript src/load_data.R 'https://catalogue.data.gov.bc.ca/dataset/5554165d-e365-422f-bf85-4f6e4c9167dc/resource/97c6cbf7-f529-464a-b771-9719855b86f6/download/fsa.csv' --arg2='data/fsa_2007-2016.csv'
+
 library(readr)
 library(docopt)
 library(testthat)
 
 opt <- docopt(doc)
+
+######################
 
 # Tests that the input link is a link to a csv file
 test_input <- function(){
@@ -33,6 +39,8 @@ test_output <- function(){
 }
 test_output()
 
+######################
+
 # Reads the data only if the link is a link to a csv file
 if (test_input() == TRUE) {
   data = read_csv(opt$arg1)
@@ -47,13 +55,10 @@ if (test_output() == TRUE){
     print("The output file should be a .csv file.")
   }
 
+######################
+
 # Alternative way to do this using commandArgs:
 # args = commandArgs(trailingOnly=TRUE)
 # data = read.csv(args[1])
 # write_csv(data, args[2])
 
-# Examples:
-# Rscript src/load_data.R 'https://catalogue.data.gov.bc.ca/dataset/5554165d-e365-422f-bf85-4f6e4c9167dc/resource/bcb547f0-8ba7-451f-9e11-10524f4d57a0/download/foundation-skills-assessment-2017-18_to_2018-19.csv' --arg2='data/fsa_2017-2018.csv'
-# Rscript src/load_data.R 'https://catalogue.data.gov.bc.ca/dataset/5554165d-e365-422f-bf85-4f6e4c9167dc/resource/97c6cbf7-f529-464a-b771-9719855b86f6/download/fsa.csv' --arg2='data/fsa_2007-2016.csv'
-
-# Note: the data folder used in this example must already exist in your project repository for the command line script to work.
